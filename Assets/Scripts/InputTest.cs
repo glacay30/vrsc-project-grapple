db1229e4275d38911246ca6082a552ef564c32bc
Gabriel Lacayo <glacayo@usc.edu>
2019-03-15 00:02:05 -0700

    controller input is now based on the controller type

--- Assets/Scripts/InputTest.cs
+++ Assets/Scripts/InputTest.cs
@@ -8,16 +8,34 @@ public class InputTest : MonoBehaviour
     public SteamVR_Action_Single shrink;
 
     private GameObject sphere = null;
+    private  SteamVR_Input_Sources source;
 
     private void Start()
     {
+        if (GetComponent<SteamVR_Behaviour_Pose>() == null)
+        {
+            Debug.LogError("No SteamVR_Behaviour_Pose set", gameObject);
+        }
 
+        source = GetComponent<SteamVR_Behaviour_Pose>().inputSource;
+        switch (source)
+        {
+            case SteamVR_Input_Sources.Any:
+                Debug.Log("Found device Any for " + ToString());
+                break;
+            case SteamVR_Input_Sources.LeftHand:
+                Debug.Log("Found device LeftHand for " + ToString());
+                break;
+            case SteamVR_Input_Sources.RightHand:
+                Debug.Log("Found device RightHand for " + ToString());
+                break;
+        }
     }
 
 
     private void Update()
     {
-        bool bSpawn = spawn.GetStateDown(SteamVR_Input_Sources.Any);
+        bool bSpawn = spawn.GetStateDown(source);
         if (bSpawn == true)
         {
             if (sphere != null)
@@ -34,7 +52,7 @@ public class InputTest : MonoBehaviour
             }
         }
 
-        float size = shrink.GetAxis(SteamVR_Input_Sources.Any);
+        float size = shrink.GetAxis(source);
         if (size > 0.1f && sphere != null)
         {
             Vector3 targetScale = new Vector3(size, size, size);
